6af539e561601194a98631d00f0a591a17b6f923
Caterina Trainito <caterina.trainito@gmail.com>
2023-03-03 12:02:13 +0100

    Support uploading multiple DICOM image files in /predict endpoint

--- app.py
+++ app.py
@@ -16,6 +16,8 @@ root_dir = "./model_weight/"
 model.load_state_dict(torch.load(os.path.join(root_dir, "best_metric_model.pth"), map_location=torch.device('cpu') if not torch.cuda.is_available() else None))
 model.eval()
 transforms = Compose([AddChannel(),NormalizeIntensity(),Resize((256,256))])
+
+
 class BrainClassificationDatasetinference(torch.utils.data.Dataset):
     def __init__(self, image_files, transforms):
         self.image_files = image_files
@@ -29,15 +31,10 @@ class BrainClassificationDatasetinference(torch.utils.data.Dataset):
         return self.transforms(self.image_files[index])
 
     
-def image_conversion(file):
-    raw = DicomBytesIO(file.read())
-    ds = dcmread(raw)
+def get_dataset(image_file):
+    raw_bytes = DicomBytesIO(image_file.read())
+    return dcmread(raw_bytes)
 
-    return ds.pixel_array
-
-
-
-    
 
 def get_prediction(test_images):
     test_images = transforms(test_images)
@@ -49,15 +46,35 @@ def get_prediction(test_images):
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    if request.method == 'POST':
-        # we will get the file from the request
-        file = image_conversion(request.files['file'])
-        
-        # convert that to bytes
-        class_name = get_prediction(file)
-        return jsonify({ 'class_name': class_name})
+    """Run brain vs. non-brain classification on one or multiple DICOM images"""
 
-    
+    if request.method == 'POST':
+        # Can supply one or multiple DICOM images (as binary files)
+        # all with the same the parameter name "file"
+        files = request.files.getlist("file")
+
+        # The API returns a list of dicts containing predictions,
+        # one dict for each input DICOM image
+        predictions = []
+
+        for file in files:
+            prediction = {k: None for k in ("series_instance_uid", "sop_instance_uid", "prediction")}
+            try:
+                ds = get_dataset(file)
+                prediction["series_instance_uid"] = ds.SeriesInstanceUID
+                prediction["sop_instance_uid"] = ds.SOPInstanceUID
+
+                class_name = get_prediction(ds.pixel_array)
+                prediction["prediction"] = class_name
+            except Exception as ex:
+                # TODO: if any one the files cannot be read/predicted,
+                # should we return HTTP status code != 200?
+                prediction["error_msg"] = str(ex)
+            finally:
+                predictions.append(prediction)
+
+        return jsonify(predictions)
+
 
 if __name__ == '__main__':
     app.run()
